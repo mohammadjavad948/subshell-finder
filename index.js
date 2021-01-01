683135d0fda0b79e12a32752eb00ed908fa890c0
mohammad javad <mjkmo123@gmail.com>
2021-01-01 18:32:56 +0330

    layers var

---
+++ index.js
@@ -0,0 +1,19 @@
+const atom = 20;
+const layers = [
+    [1, 's', 2],
+    [2, 's', 2],
+    [2, 'p', 6],
+    [3, 's', 2],
+    [3, 'p', 6],
+    [4, 's', 2],
+    [3, 'd', 10],
+    [4, 'p', 6],
+    [5, 's', 2],
+    [4, 'd', 10],
+    [5, 'p', 6]
+];
+
+function find(atomNumber) {
+
+    function layers(number){}
+}
